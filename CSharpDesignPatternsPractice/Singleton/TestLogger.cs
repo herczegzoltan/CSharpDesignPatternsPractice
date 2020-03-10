2e4fcbd4316d26acd9f36f7bb49e8b445fa0e336
Zoltan Herczeg <zoltan.herczeg>
2020-03-10 15:10:00 +0100

    Singleton Example

---
+++ CSharpDesignPatternsPractice/Singleton/TestLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpDesignPatternsPractice.Singleton
+{
+    class TestLogger
+    {
+        static void Main()
+        {
+            Logger ob1 = Logger.GetInstance();
+            
+            Logger ob2 = Logger.GetInstance();
+
+
+            //Same hashcode
+            Console.WriteLine(ob1.GetHashCode());
+            Console.WriteLine(ob2.GetHashCode());
+        }
+    }
+}
