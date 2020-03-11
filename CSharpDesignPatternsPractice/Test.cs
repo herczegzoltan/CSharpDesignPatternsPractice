9f95029f270324a0860fe4d3bc67e7e8f8d5b1ff
Zoltan Herczeg <zoltan.herczeg>
2020-03-11 11:30:14 +0100

    Refactoring

--- CSharpDesignPatternsPractice/Test.cs
+++ CSharpDesignPatternsPractice/Test.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpDesignPatternsPractice.Singleton;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,21 @@ namespace CSharpDesignPatternsPractice.Factory
     {
         static void Main()
         {
+            //Factory Pattern
             Console.WriteLine("Enter Add, subtract or divide");
 
             CalculateFactory factory = new CalculateFactory();
             ICalculate obj = factory.GetCalculation(Console.ReadLine());
             obj.Calculate(10, 10);
+
+
+            //Singleton
+            Logger ob1 = Logger.Instance;
+
+            Logger ob2 = Logger.Instance;
+            //Same hashcode
+            Console.WriteLine(ob1.GetHashCode());
+            Console.WriteLine(ob2.GetHashCode());
         }
     }
 }
